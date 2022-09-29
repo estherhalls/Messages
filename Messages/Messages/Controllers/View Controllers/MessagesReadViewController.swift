6e88a7d4c73dfb46d595ff0dfdba9d121154d657
Esther <esther@Esther-Halls-MacBook-Pro.local>
2022-09-29 11:10:11 -0600

    adjusted tab bar visual settings

---
+++ Messages/Messages/Controllers/View Controllers/MessagesReadViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MessagesReadViewController.swift
+//  Messages
+//
+//  Created by Esther on 9/29/22.
+//
+
+import UIKit
+
+class MessagesReadViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
